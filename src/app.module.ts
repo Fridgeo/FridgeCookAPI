02df0938a50bf065658120abd0727eca0d9e33dc
SmasBalloon <mathissauvage24000@gmail.com>
2026-01-08 14:22:01 +0000

    feat: ajouter le module Recette dans AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { RecetteModule } from './recette/recette.module';
 @Module({
-  imports: [],
+  imports: [RecetteModule],
   controllers: [],
   providers: [PrismaService],
 })
